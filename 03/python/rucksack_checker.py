8e2e2775138069fdf20058e51956fc375655ca10
rolkon <44371104+rolkon@users.noreply.github.com>
2022-12-04 15:17:32 +0100

    solved day 3

---
+++ 03/python/rucksack_checker.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+priorities = {}
+
+for i, letter in enumerate(alphabet):
+	priorities[letter] = i+1
+
+def get_common_priority(string_list):
+
+	priority_lists = [[priorities[letter] for letter in string] for string in string_list]
+	sorted_priority_sets = [sorted(set(priority_list)) for priority_list in priority_lists]
+
+	# 53 elements because priorities start at 1
+	occurence_map = np.zeros([len(string_list), 53], dtype=bool)
+
+	for i in range(len(sorted_priority_sets)):
+		occurence_map[i, sorted_priority_sets[i]] = True
+
+	occurence_map = np.logical_and.reduce(occurence_map, axis=0)
+
+	return occurence_map.nonzero()[0][0]
+
+
+with open('../input.txt') as file:
+
+	rucksacks = file.read().split('\n')
+
+	# part 1
+	sum_priorities = 0
+
+	for rucksack in rucksacks:
+		left_side = rucksack[:len(rucksack)//2]
+		right_side = rucksack[len(rucksack)//2:]
+
+		sum_priorities += get_common_priority([left_side, right_side])
+
+	print("Part 1 result: ", sum_priorities)
+
+	# part 2
+	sum_priorities = 0
+
+	for i in range(0, len(rucksacks), 3):
+		sum_priorities += get_common_priority(rucksacks[i:i+3])
+
+	print("Part 2 result: ", sum_priorities)
